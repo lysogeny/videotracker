f00b461728e2168df81b522e9af9d7b40fab2735
Jooa <eru@jooa.xyz>
2019-04-09 10:18:07 +0200

    Change argparser to be created not subclassed

--- videotracker/cli.py
+++ videotracker/cli.py
@@ -2,19 +2,21 @@
 
 import argparse
 
-class Parser(argparse.ArgumentParser):
-    """Argument parser for videotracker"""
-    def __init__(self, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
-        self.add_argument('-o', '--open',
-                          nargs='?', default=None,
-                          help='Open a specific file')
-        self.add_argument('-c', '--csv',
-                          nargs='?', default=None,
-                          help='Write CSV to a specific file')
-        self.add_argument('-v', '--vid',
-                          nargs='?', default=None,
-                          help='Write video to a specific file')
-        self.add_argument('-m', '--module',
-                          nargs='?', default=None,
-                          help='Loads a specific module')
+#class Parser(argparse.ArgumentParser):
+#    """Argument parser for videotracker"""
+#    def __init__(self, *args, **kwargs):
+#        super().__init__(self, *args, **kwargs)
+#
+parser = argparse.ArgumentParser()
+parser.add_argument('-o', '--open',
+                    nargs='?', default=None,
+                    help='Open a specific file')
+parser.add_argument('-c', '--csv',
+                    nargs='?', default=None,
+                    help='Write CSV to a specific file')
+parser.add_argument('-v', '--vid',
+                    nargs='?', default=None,
+                    help='Write video to a specific file')
+parser.add_argument('-m', '--module',
+                    nargs='?', default=None,
+                    help='Loads a specific module')
