ce064af0467c682d4907e7db2e5f4c82a5f933e6
Jooa <eru@jooa.xyz>
2019-04-24 01:44:18 +0200

    Initial changes to property structure

--- videotracker/properties.py
+++ videotracker/properties.py
@@ -1,9 +1,9 @@
 """Definitions for properties"""
 
-from typing import Callable
+from typing import Callable, Tuple
 from dataclasses import dataclass
 
-from PyQt5.QtWidgets import QWidget, QLabel, QHBoxLayout
+from PyQt5.QtWidgets import QWidget, QLabel, QHBoxLayout, QSpinBox, QDoubleSpinBox
 
 @dataclass
 class BaseArgument:
@@ -56,3 +56,56 @@ class Argument(BaseArgument):
         self.widget = QWidget()
         self.widget.valueChanged = self.value_widget.valueChanged
         self.widget.setLayout(layout)
+
+class IntegerArgument(BaseArgument):
+    """Argument that is of type int"""
+    # This contains a QSpinBox with a maximum range and minimum range
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.widget = QSpinBox()
+
+    @property
+    def range(self) -> Tuple[int, int]:
+        """Range of the spinbox"""
+        return (self.widget.minimum(), self.widget.maximum())
+    @range.setter
+    def range(self, value: Tuple[int, int]):
+        self.widget.setRange(value)
+
+    @property
+    def minimum(self) -> int:
+        """Minimum value possible in the spinbox"""
+        return self.widget.minimum()
+    @minimum.setter
+    def minimum(self, value: int):
+        """Minimum value possible in the spinbox"""
+        return self.widget.setMinimum(value)
+
+    @property
+    def maximum(self) -> int:
+        """Maximum value possible in the spinbox"""
+        return self.widget.maximum()
+    @maximum.setter
+    def maximum(self, value: int):
+        """Maximum value possible in the spinbox"""
+        return self.widget.setMaximum(value)
+
+    @property
+    def step(self) -> int:
+        """Allowable steps of this element"""
+        return self.widget.singleStep()
+    @step.setter
+    def step(self, value: int):
+        """Allowable steps of this element"""
+        return self.widget.setSingleStep(value)
+
+class FloatArgument(BaseArgument):
+    """"Argument that is of type `float`"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.widget = QDoubleSpinBox()
+
+
+class MultipleChoiceArgument(BaseArgument):
+    """Argument with multiple options"""
+    # We need a mapping of options to values for the function. Also pretty names and something?
